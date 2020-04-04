e1cfeae76b90bbf265eb9fc510f5afb3c7ef1fa5
tharindu4903 <tharindudhananjaya66@gmail.com>
2020-04-05 01:25:28 +0530

    client page ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Navbar,InputGroup,FormControl,Form,Button,Col} from 'react-bootstrap';
+import './index.css';
+import logo from "./13.png";
+ 
 
-function App() {
+class App extends Component {
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Navbar collapseOnSelect expand="lg"  bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        <img
+          alt="fg"
+          src={logo}
+          width="50"
+          height="30"
+         
+        />{' '}
+        D'Front Creations Invoice Genarator
+      </Navbar.Brand>
+    </Navbar>
+    <div>
+    <InputGroup className="mb-3">
+      <InputGroup.Prepend>
+        <InputGroup.Text id="basic-addon1">#</InputGroup.Text>
+      </InputGroup.Prepend>
+      <FormControl
+        placeholder="invoice no"
+        aria-label="invoice no"
+        aria-describedby="basic-addon1"
+      />
+    </InputGroup>
+  
+    <InputGroup className="mb-3">
+      <FormControl
+        placeholder="Customer Name"
+        aria-label="Customer Name"
+        aria-describedby="basic-addon2"
+      />
+    
+    </InputGroup>
     </div>
+    <Form>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Description</Form.Label>
+      <Form.Control type="description" placeholder="Description" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Quntity</Form.Label>
+      <Form.Control type="quntity" placeholder="Quntity" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Price</Form.Label>
+      <Form.Control type="price" placeholder="Price" />
+    </Form.Group>
+  </Form.Row>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Control type="description" placeholder="Description" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="quntity" placeholder="Quntity" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="price" placeholder="Price" />
+    </Form.Group>
+  </Form.Row>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Control type="description" placeholder="Description" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="quntity" placeholder="Quntity" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="price" placeholder="Price" />
+    </Form.Group>
+  </Form.Row>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Control type="description" placeholder="Description" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="quntity" placeholder="Quntity" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="price" placeholder="Price" />
+    </Form.Group>
+  </Form.Row>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Control type="description" placeholder="Description" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="quntity" placeholder="Quntity" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="price" placeholder="Price" />
+    </Form.Group>
+  </Form.Row>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Control type="description" placeholder="Description" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="quntity" placeholder="Quntity" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Control type="price" placeholder="Price" />
+    </Form.Group>
+  </Form.Row>
+
+  <Button variant="primary" type="submit">
+    Create Invoice
+  </Button>
+</Form>
+    </div>
+
   );
 }
-
+}
 export default App;
