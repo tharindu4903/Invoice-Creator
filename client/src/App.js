953be21ade1861fb98deeaa56c9eb7aa3dd71dc1
tharindu4903 <tharindudhananjaya66@gmail.com>
2020-04-07 00:13:12 +0530

    html ok

--- client/src/App.js
+++ client/src/App.js
@@ -10,222 +10,243 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-                  invoiceNo: '',
-                  customerName: '',
-                  des1: '',
-                  qty1: 10,
-                  pr1: 100,
-                  tot1: 0,
-                  des2: '',
-                  qty2: 0,
-                  pr2: 0,
-                  tot2: 0,
-                  des3: '',
-                  qty3: 0,
-                  pr3: 0,
-                  tot3: 0,
-                  des4: '',
-                  qty4: 0,
-                  pr4: 0,
-                  tot4: 0,
-                  des5: '',
-                  qty5: 0,
-                  pr5: 0,
-                  tot5: 0,
-                  des6: '',
-                  qty6: 0,
-                  pr6: 0,
-                  tot6: 0,
-                  subTot: 0,
-                  discount: 100,
-                  total: 0,
-                  
-                }
-   }
-   
-   handleChange = (event) => {
+      invoiceNo: 1000000,
+      customerName: '',
+      des1: '',
+      qty1: '',
+      pr1: '',
+      tot1: '',
+      des2: '',
+      qty2: '',
+      pr2: '',
+      tot2: '',
+      des3: '',
+      qty3: '',
+      pr3: '',
+      tot3: '',
+      des4: '',
+      qty4: '',
+      pr4: '',
+      tot4: '',
+      des5: '',
+      qty5: '',
+      pr5: '',
+      tot5: '',
+      des6: '',
+      qty6: '',
+      pr6: '',
+      tot6: '',
+      total: '',
+      discount: '',
+
+
+    }
+  }
+
+  handleChange = (event) => {
     const { target: { name, value } } = event
     this.setState({ [name]: value })
+
   }
-    
-    tot1 = () => this.state.pr1 * this.state.qty1;
-    tot1=this.tot1;
-    tot2 = () => this.state.pr2 * this.state.qty2;
-    tot2=this.tot2;
-    tot3 = () => this.state.pr3 * this.state.qty3;
-    tot3=this.tot3;
-    tot4 = () => this.state.pr4 * this.state.qty4;
-    tot4=this.tot4;
-    tot5 = () => this.state.pr5 * this.state.qty5;
-    tot5=this.tot5;
-    tot6 = () => this.state.pr6 * this.state.qty6;
-    tot6=this.tot6;
 
-    total = () =>(this.state.tot1 + this.state.tot2 + this.state.tot3 + this.state.tot4 + this.state.tot5 + this.state.tot6);
-    total=this.total;
 
-    createAndDownloadPdf = () => {
-      axios.post('/create-pdf', this.state)
-        .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
-        .then((res) => {
-          const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+  createAndDownloadPdf = () => {
+    axios.post('/create-pdf', this.state)
+      .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
 
-          saveAs(pdfBlob, 'newPdf.pdf');
-        })
-    }
+        saveAs(pdfBlob, 'newPdf.pdf');
+      })
 
-  
-    render() {
-      return (
-        <div >
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">
-              <img
-                alt="fg"
-                src={logo}
-                width="50"
-                height="30"
+  }
 
-              />{' '}
+  calculation = () => {
+    this.setState({ tot1: this.state.pr1 * this.state.qty1 });
+    this.setState({ tot2: this.state.pr2 * this.state.qty2 });
+    this.setState({ tot3: this.state.pr3 * this.state.qty3 });
+    this.setState({ tot4: this.state.pr4 * this.state.qty4 });
+    this.setState({ tot5: this.state.pr5 * this.state.qty5 });
+    this.setState({ tot6: this.state.pr6 * this.state.qty6 });
+    this.setState({ total: this.state.tot1 + this.state.tot2 + this.state.tot3 + this.state.tot4 + this.state.tot5 + this.state.tot6 });
+
+  }
+  all = (event) => {
+    this.handleChange(event)
+    this.calculation()
+
+  }
+
+
+  render() {
+    return (
+      <div >
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt="fg"
+              src={logo}
+              width="30"
+              height="30"
+
+            />{' '}
         D'Front Creations Invoice Genarator
       </Navbar.Brand>
-          </Navbar>
+        </Navbar>
 
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">#</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="invoice no"
+            name="invoiceNo"
+            value={this.state.name}
+            onChange={this.handleChange}
+            aria-label="invoiceno"
+            aria-describedby="basic-addon1"
+
+          />
+        </InputGroup>
+
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Customer Name"
+            name="customerName"
+            value={this.state.name}
+            onChange={this.handleChange}
+            aria-label="CustomerName"
+            aria-describedby="basic-addon2"
+
+          />
+
+        </InputGroup>
+
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Description</Form.Label>
+              <Form.Control type="description" placeholder="Description" name="des1" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Quntity</Form.Label>
+              <Form.Control type="quntity" placeholder="Quntity" name="qty1" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="price" placeholder="Price" name="pr1" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Control type="description" placeholder="Description" name="des2" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="quntity" placeholder="Quntity" name="qty2" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="price" placeholder="Price" name="pr2" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Control type="description" placeholder="Description" name="des3" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="quntity" placeholder="Quntity" name="qty3" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="price" placeholder="Price" name="pr3" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Control type="description" placeholder="Description" name="des4" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="quntity" placeholder="Quntity" name="qty4" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="price" placeholder="Price" name="pr4" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Control type="description" placeholder="Description" name="des5" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="quntity" placeholder="Quntity" name="qty5" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="price" placeholder="Price" name="pr5" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Control type="description" placeholder="Description" name="des6" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="quntity" placeholder="Quntity" name="qty6" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Control type="price" placeholder="Price" name="pr6" value={this.state.name}
+                onChange={this.all} />
+            </Form.Group>
+          </Form.Row>
           <InputGroup className="mb-3">
             <InputGroup.Prepend>
-              <InputGroup.Text id="basic-addon1">#</InputGroup.Text>
+              <InputGroup.Text id="basic-addon1">Rs.</InputGroup.Text>
             </InputGroup.Prepend>
             <FormControl
-              placeholder="invoice no"
-              name="invoiceNo"
+              placeholder="Discount"
+              name="discount"
               value={this.state.name}
-              onChange={this.handleChange} 
-              aria-label="invoiceno"
+              onChange={this.all}
+              aria-label="discount"
               aria-describedby="basic-addon1"
 
             />
           </InputGroup>
 
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="Customer Name"
-              name="customerName"
-              value={this.state.name}
-              onChange={this.handleChange} 
-              aria-label="CustomerName"
-              aria-describedby="basic-addon2"
 
-            />
-
-          </InputGroup>
-
-          <Form>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Description</Form.Label>
-                <Form.Control type="description" placeholder="Description" name="des1" value={this.state.name}
-              onChange={this.handleChange}  />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Quntity</Form.Label>
-                <Form.Control type="quntity" placeholder="Quntity" name="qty1" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Price</Form.Label>
-                <Form.Control type="price" placeholder="Price" name="pr1" onChange={this.handleChange} />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Control type="description" placeholder="Description" name="des2" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="quntity" placeholder="Quntity" name="qty2" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="price" placeholder="Price" name="pr2" onChange={this.handleChange} />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Control type="description" placeholder="Description" name="des3" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="quntity" placeholder="Quntity" name="qty3" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="price" placeholder="Price" name="pr3" onChange={this.handleChange} />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Control type="description" placeholder="Description" name="des4" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="quntity" placeholder="Quntity" name="qty4" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="price" placeholder="Price" name="pr4" onChange={this.handleChange} />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Control type="description" placeholder="Description" name="des5" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="quntity" placeholder="Quntity" name="qty5" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="price" placeholder="Price" name="pr5" onChange={this.handleChange} />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Control type="description" placeholder="Description" name="des6" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="quntity" placeholder="Quntity" name="qty6" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Control type="price" placeholder="Price" name="pr6" onChange={this.handleChange} />
-              </Form.Group>
-            </Form.Row>
-            <InputGroup className="mb-3">
-              <InputGroup.Prepend>
-                <InputGroup.Text id="basic-addon1">Rs.</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl
-                placeholder="Discount"
-                name="discount"
-                onChange={this.handleChange}
-                aria-label="discount"
-                aria-describedby="basic-addon1"
-
-              />
-            </InputGroup>
-
-
-            <Button variant="primary" type="submit" onClick={this.createAndDownloadPdf}>
-              Create Invoice
+          <Button variant="primary" type="submit" onClick={this.createAndDownloadPdf} >
+            Create Invoice
             </Button>
-          </Form>
-        </div>
+        </Form>
+      </div>
 
-      );
-    }
-    
+    );
   }
 
-  export default App;
+}
+
+export default App;
--- server/documents/index.js
+++ server/documents/index.js
@@ -1,6 +1,30 @@
 
-module.exports = ({ invoiceNo,customerName ,des1 ,qty1 ,pr1,tot1 ,des2 ,qty2 ,pr2,tot2 ,des3 ,qty3 ,pr3,tot3 ,des4 ,qty4 ,pr4 ,tot4,des5 ,qty5 ,pr5,tot5 ,des6 ,qty6 ,pr6 ,tot6,discount,total}) => {
+module.exports = ({ invoiceNo,customerName ,des1 ,qty1 ,pr1,tot1 ,des2 ,qty2 ,pr2,tot2 ,des3 ,qty3 ,pr3,tot3 ,des4 ,qty4 ,pr4 ,tot4,des5 ,qty5 ,pr5,tot5 ,des6 ,qty6 ,pr6 ,tot6,total,discount}) => {
     const today = new Date();
+   if (tot2!=0) {
+       tot2=tot2;
+    } else 
+       tot2='';
+      
+       if (tot3===0) {
+          tot3="";
+       } else 
+          tot3=tot3;
+          if (tot4===0) {
+            tot4="";
+         } else 
+            tot4=tot4;
+            if (tot5===0) {
+               tot5="";
+            } else 
+               tot5=tot5;
+               if (tot6===0) {
+                  tot6="";
+               } else 
+                  tot6=tot6;
+                   
+
+    
 return `
 <!doctype html>
 <html>
@@ -8,111 +32,124 @@ return `
       <meta charset="utf-8">
       <title>D'Front Creations</title>
       <style>
-         .frame{           
-         height:1100px;
-         width: 750px;           
+      .frame {
+         height: 1100px;
+         width: 750px;
          max-width: 750px;
          margin: auto;
-         padding: 30px;
+         
          background-image: url("https://github.com/tharindu4903/invoice/blob/master/invoice%20temp.jpg?raw=true");
          background-repeat: no-repeat;
          background-position: center;
          background-size: cover;
-       
-         }
-          
-         .invoice-box {
+
+      }
+
+      .invoice-box {
          position: relative;
-         top: -32px;
-         left: -30px;
+        
          width: 750px;
          height: 1100px;
          max-width: 750px;
-         padding: 30px;
+        
          box-shadow: 0 0 10px rgba(0, 0, 0, .15);
          font-size: 16px;
          line-height: 24px;
          font-family: 'Helvetica Neue', 'Helvetica';
          color: #555;
-         }
-        
-         .justify-center {
+      }
+
+      .justify-center {
          text-align: center;
-         }
-        table {
-           position: relative;
-           top: 360px;
-           left: -4px;
-           border-collapse: collapse;
-           width: 90.5%;
-        }
-        th,td {
-           padding: 11.9px;
-        }
-        .details{
-           position: relative;
-           top: 244px;
-           left: 528px;
-           
-        }
-        .details p{
-           margin: .5px;
-        
+      }
 
-        }
-        .price{
-           position: relative;
-           top: 382px;
-           right:49px;    
-        }
-        .price p{
-           margin: 4px;
-           text-align: right;
-        }
-        .fullAmount{
-           position: relative;
-           top: 407px;
-           right:49px;    
-        }
-        .fullAmount p{
-           margin: 4px;
-           text-align: right;
-        }
-        .to{
-           position: relative;
-           top: 330px;
-           left: 52px;    
-        }
-        .to p{
-           margin: 4px;
-           text-align: right;
-        }
-        .hide{
-         display: none;
-        }
+      table {
+         position: relative;
+         top: 360px;
+         left: -4px;
+         border-collapse: collapse;
+         width: 87%;
+      }
 
-         @media only screen and (max-width: 600px) {
+      th,
+      td {
+         padding: 11.9px;
+      }
+
+      .details {
+         position: relative;
+         top: 258px;
+         left: 520px;
+
+      }
+
+      .details p {
+         margin: .2px;
+
+
+      }
+
+      .price {
+         position: relative;
+         top: 369px;
+         right: 60px;
+      }
+
+      .price p {
+         margin: 4px;
+         text-align: right;
+      }
+
+      .fullAmount {
+         position: relative;
+         top: 390px;
+         right: 60px;
+      }
+
+      .fullAmount p {
+         margin: 4px;
+         text-align: right;
+      }
+
+      .to {
+         position: relative;
+         top: 315px;
+         left: 70px;
+      }
+
+      .to p {
+         margin: 4px;
+         text-align: right;
+      }
+
+      .tablebox {
+         width: 100%;
+         height: 280px;
+      }
+
+      @media only screen and (max-width: 600px) {
          .invoice-box table tr.top table td {
-         width: 100%;
-         
-         text-align: center;
+            width: 100%;
+
+            text-align: center;
          }
+
          .invoice-box table tr.information table td {
-         width: 100%;
-        
-         text-align: center;
+            width: 100%;
+
+            text-align: center;
          }
-         }
-      </style>
+      }
+   </style>
    </head>
    <body>
       <div class="frame">
       <div class="invoice-box">
        <div class="details">
-        <p style="text-align: right: ;">${invoiceNo}</p>
-        <p style="text-align: right: ;">${`${today.getDate()}. ${today.getMonth() + 1}. ${today.getFullYear()}.`}</p>
+        <p >${invoiceNo}</p>
+        <p >${`0${today.getDate()}. 0${today.getMonth() + 1}. ${today.getFullYear()}`}</p>
      </div>
-     <div>
+     <div class="tablebox">
          <table align="center" >
             <tr>
                <td width=50%>${des1}</td>
